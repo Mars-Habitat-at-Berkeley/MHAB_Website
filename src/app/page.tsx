8ab30c10632747e2ebd16291530efec79af0c517
Yonatan <yherrera0044@mymail.lausd.net>
2024-09-19 15:30:51 -0700

    project setup :)

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+// import Image from 'next/image';
+
+// import Profile from './components/test';
+// import { Head } from 'next/head';
+export default function Home() {
+  return <div>{/* <Profile /> */}</div>;
+}
