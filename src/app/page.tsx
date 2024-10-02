bd335d7f6d18ed260c83a531e2d3713f77a264d8
Yonatan <yonatan.herrera001@gmail.com>
2024-10-02 00:28:05 -0700

    added the background, working on the text :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.css';
 import Background from './components/background';
-
+import IntroSection from './components/introSection';
 // import './global.css';
 // import Profile from './components/test';
 // import { Head } from 'next/head';
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <Background />
+      <IntroSection />
     </div>
   );
 }
